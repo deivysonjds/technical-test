c62ad471416a2c69df8d27f6fae6718b5e5b536c
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-07 22:18:32 -0300

    feat: added Supplier abstract model

--- backend/src/main/java/com/br/technical_test/entity/Supplier.java
+++ backend/src/main/java/com/br/technical_test/entity/Supplier.java
@@ -1,4 +1,26 @@
 package com.br.technical_test.entity;
 
-public class Supplier {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String cep;
+
 }
