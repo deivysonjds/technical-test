05a947e9ec0f9ce26df20ab19e90fdbceb8a2f13
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-09 04:50:21 -0300

    feat: services to enterprise model

--- backend/src/main/java/com/br/technical_test/service/EnterpriseService.java
+++ backend/src/main/java/com/br/technical_test/service/EnterpriseService.java
@@ -1,9 +1,14 @@
-package com.br.technical_test;
+package com.br.technical_test.service;
 
+import com.br.technical_test.dto.request.EnterpriseRequest;
+import com.br.technical_test.dto.response.EnterpriseResponse;
 import com.br.technical_test.entity.Enterprise;
 import com.br.technical_test.exception.NoSuchResource;
+import com.br.technical_test.mapper.EnterpriseMapper;
 import com.br.technical_test.repository.EnterpriseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,23 +21,46 @@ public class EnterpriseService {
     @Autowired
     private EnterpriseRepository enterpriseRepository;
 
-    @Transactional(readOnly = true)
-    public List<Enterprise> findAll(){
-        return enterpriseRepository.findAll();
+    @Autowired
+    private EnterpriseMapper enterpriseMapper;
+
+    @Transactional
+    public EnterpriseResponse insert(EnterpriseRequest enterpriseRequest){
+        Enterprise enterprise = enterpriseMapper.toEntity(enterpriseRequest);
+        enterprise = enterpriseRepository.save(enterprise);
+        return enterpriseMapper.toDto(enterprise);
     }
 
     @Transactional(readOnly = true)
-    public Enterprise findById(Long id){
+    public Page<EnterpriseResponse> findAll(Pageable pageable){
+        return enterpriseRepository.findAll(pageable).map(enterpriseMapper::toDto);
+    }
+
+    @Transactional(readOnly = true)
+    public EnterpriseResponse findById(Long id){
         Optional<Enterprise> enterprise = enterpriseRepository.findById(id);
         if (enterprise.isEmpty()) throw new NoSuchResource("enterprise");
-        return enterprise.get();
+
+        return enterpriseMapper.toDto(enterprise.get());
     }
 
     @Transactional(readOnly = true)
-    public Enterprise findByCnpj(String cnpj){
+    public EnterpriseResponse findByCnpj(String cnpj){
         Optional<Enterprise> enterprise = enterpriseRepository.findByCnpj(cnpj);
         if (enterprise.isEmpty()) throw new NoSuchResource("enterprise");
-        return enterprise.get();
+
+        return enterpriseMapper.toDto(enterprise.get());
+    }
+
+    @Transactional
+    public EnterpriseResponse updateById(Long id, EnterpriseRequest enterpriseRequest){
+        if (!enterpriseRepository.existsById(id)) throw new NoSuchResource("enterprise");
+
+        Enterprise enterprise = enterpriseMapper.toEntity(enterpriseRequest);
+        enterprise.setId(id);
+        enterprise = enterpriseRepository.save(enterprise);
+
+        return enterpriseMapper.toDto(enterprise);
     }
 
     @Transactional
