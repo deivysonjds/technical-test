bbac591ebc37187ed3e713847295e0417b8c54d3
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-09 04:50:43 -0300

    feat: mapper to enterprise model

--- backend/src/main/java/com/br/technical_test/mapper/EnterpriseMapper.java
+++ backend/src/main/java/com/br/technical_test/mapper/EnterpriseMapper.java
@@ -1,4 +1,15 @@
 package com.br.technical_test.mapper;
 
+import com.br.technical_test.dto.request.EnterpriseRequest;
+import com.br.technical_test.dto.response.EnterpriseResponse;
+import com.br.technical_test.entity.Enterprise;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface EnterpriseMapper {
+    EnterpriseResponse toDto(Enterprise enterprise);
+    Enterprise toEntity(EnterpriseRequest enterpriseRequest);
+    List<EnterpriseResponse> toDtoList(List<Enterprise> enterprises);
 }
