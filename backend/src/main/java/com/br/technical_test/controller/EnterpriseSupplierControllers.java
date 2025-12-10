cdda432bc537dde60f616146ac3577980dc5a661
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-10 17:19:21 -0300

    feat: association controller

--- backend/src/main/java/com/br/technical_test/controller/EnterpriseSupplierControllers.java
+++ backend/src/main/java/com/br/technical_test/controller/EnterpriseSupplierControllers.java
@@ -1,4 +1,29 @@
 package com.br.technical_test.controller;
 
+import com.br.technical_test.dto.request.EnterpriseSupplierRequest;
+import com.br.technical_test.dto.response.EnterpriseSupplierResponse;
+import com.br.technical_test.service.EnterpriseSupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/association")
 public class EnterpriseSupplierControllers {
+
+    @Autowired
+    private EnterpriseSupplierService enterpriseSupplierService;
+
+    @PostMapping("/")
+    public ResponseEntity<EnterpriseSupplierResponse> associate(@RequestBody EnterpriseSupplierRequest enterpriseSupplierRequest){
+        try{
+            EnterpriseSupplierResponse enterpriseSupplierResponse = enterpriseSupplierService.associate(enterpriseSupplierRequest);
+            return ResponseEntity.ok().body(enterpriseSupplierResponse);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
