76d7cede8fee4fc35ab27c1e89baa078b0424587
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-10 04:36:20 -0300

    refactor: added notation

--- backend/src/main/java/com/br/technical_test/entity/Enterprise.java
+++ backend/src/main/java/com/br/technical_test/entity/Enterprise.java
@@ -1,6 +1,7 @@
 package com.br.technical_test.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Enterprise {
 
     @Id
