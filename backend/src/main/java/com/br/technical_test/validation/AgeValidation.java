5af2b497dfd09f78b8a32b0923eed74764531ad2
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-10 17:11:09 -0300

    feat: age validation

--- backend/src/main/java/com/br/technical_test/validation/AgeValidation.java
+++ backend/src/main/java/com/br/technical_test/validation/AgeValidation.java
@@ -1,4 +1,10 @@
 package com.br.technical_test.validation;
 
+import java.time.LocalDate;
+
 public class AgeValidation {
+
+    public static boolean isLegalAge(LocalDate birthDate){
+        return !birthDate.plusYears(18).isAfter(LocalDate.now());
+    }
 }
