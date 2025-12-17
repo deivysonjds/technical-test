f4037a5a747b90028cbe44b0cbcfbfcc1699e6d5
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-17 01:04:22 -0300

    feat: suppliers types

---
+++ frontend/types/supplier.ts
@@ -0,0 +1,17 @@
+
+
+export interface supplierPfRequest {
+    name: string,
+    email: string,
+    cep: string,
+    cpf: string,
+    rg: string,
+    birth: Date
+}
+
+export interface supplierPfRequest {
+    name: string,
+    email: string,
+    cep: string,
+    cnpj: string,
+}
