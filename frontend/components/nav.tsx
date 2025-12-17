d9fa401504b70b5861b51bad71d0d386e3bb98b2
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-17 01:21:31 -0300

    feat: navbar

--- frontend/components/nav.tsx
+++ frontend/components/nav.tsx
@@ -4,9 +4,9 @@ export default function Nav(){
 
     return (
         <>
-            <nav>
-                <a href="/">Empresas</a>
-                <a href="/fornecedores"></a>
+            <nav className="flex flex-row justify-end items-center gap-10 pr-15 h-15 shadow">
+                <a className="text-lg hover:scale-105 hover:text-blue-400 font-bold" href="/">Empresas</a>
+                <a className="text-lg hover:scale-105 hover:text-blue-400 font-bold" href="/fornecedores">Fornecedores</a>
             </nav>
         </>
     )
