e88321ddd11483ce127d4bd440c43e58cdbd2bea
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-19 01:24:08 -0300

    feat: full and summarized serialization separation

--- backend/src/main/java/com/br/technical_test/mapper/EnterpriseMapper.java
+++ backend/src/main/java/com/br/technical_test/mapper/EnterpriseMapper.java
@@ -1,15 +1,44 @@
 package com.br.technical_test.mapper;
 
 import com.br.technical_test.dto.request.EnterpriseRequest;
-import com.br.technical_test.dto.response.EnterpriseResponse;
-import com.br.technical_test.entity.Enterprise;
+import com.br.technical_test.dto.response.*;
+import com.br.technical_test.entity.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = SupplierMapper.class)
 public interface EnterpriseMapper {
+
+    @Mapping(
+            target = "suppliers",
+            expression = "java(mapSuppliers(enterprise.getSuppliers()))"
+    )
     EnterpriseResponse toResponse(Enterprise enterprise);
+    EnterpriseSummaryResponse toSummaryResponse(Enterprise enterprise);
+
     Enterprise toEntity(EnterpriseRequest enterpriseRequest);
-    List<EnterpriseResponse> toDtoList(List<Enterprise> enterprises);
+
+    SupplierPFSummaryResponse toPFSummaryResponse(SupplierPF supplierPF);
+    SupplierPJSummaryResponse toPJSummaryResponse(SupplierPJ supplier);
+
+    default List<SupplierSummaryResponse> mapSuppliers(
+            List<EnterpriseSupplier> suppliers
+    ) {
+        if (suppliers == null || suppliers.isEmpty()) {
+            return List.of();
+        }
+
+        return suppliers.stream()
+                .map(EnterpriseSupplier::getSupplier)
+                .map(supplier -> {
+                    if (supplier instanceof SupplierPF) return toPFSummaryResponse((SupplierPF) supplier);
+                    if (supplier instanceof SupplierPJ) return toPJSummaryResponse((SupplierPJ) supplier);
+                    throw new IllegalArgumentException("Unknown Supplier subtype: " + supplier.getClass());
+                })
+                .toList();
+    }
+
 }
+
--- backend/src/main/java/com/br/technical_test/mapper/SupplierMapper.java
+++ backend/src/main/java/com/br/technical_test/mapper/SupplierMapper.java
@@ -2,13 +2,12 @@ package com.br.technical_test.mapper;
 
 import com.br.technical_test.dto.request.SupplierPFRequest;
 import com.br.technical_test.dto.request.SupplierPJRequest;
-import com.br.technical_test.dto.response.SupplierPFResponse;
-import com.br.technical_test.dto.response.SupplierPJResponse;
-import com.br.technical_test.dto.response.SupplierResponse;
-import com.br.technical_test.entity.Supplier;
-import com.br.technical_test.entity.SupplierPF;
-import com.br.technical_test.entity.SupplierPJ;
+import com.br.technical_test.dto.response.*;
+import com.br.technical_test.entity.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SupplierMapper {
@@ -17,9 +16,36 @@ public interface SupplierMapper {
 
     SupplierPFResponse toPFResponse(SupplierPF supplier);
     SupplierPJResponse toPJResponse(SupplierPJ supplier);
+    @Mapping(
+            target = "suppliers",
+            expression = "java(mapEnterprises(supplier.getEnterprises()))"
+    )
     default SupplierResponse toResponse(Supplier supplier){
         if (supplier instanceof SupplierPF) return toPFResponse((SupplierPF) supplier);
         if (supplier instanceof SupplierPJ) return toPJResponse((SupplierPJ) supplier);
+        throw new IllegalArgumentException("Unknown Supplier subtype: " + supplier.getClass());
+    }
+
+    EnterpriseSummaryResponse toEnterpriseSummaryResponse(Enterprise enterprise);
+
+    default List<EnterpriseSummaryResponse> mapEnterprises(
+            List<EnterpriseSupplier> enterprises
+    ){
+        if (enterprises == null || enterprises.isEmpty()) {
+            return List.of();
+        }
+
+        return enterprises.stream()
+                .map(EnterpriseSupplier::getEnterprise)
+                .map(this::toEnterpriseSummaryResponse)
+                .toList();
+    }
+
+    SupplierPFSummaryResponse toPFSummaryResponse(SupplierPF supplier);
+    SupplierPJSummaryResponse toPJSummaryResponse(SupplierPJ supplier);
+    default SupplierSummaryResponse toSummaryResponse(Supplier supplier){
+        if (supplier instanceof SupplierPF) return toPFSummaryResponse((SupplierPF) supplier);
+        if (supplier instanceof SupplierPJ) return toPJSummaryResponse((SupplierPJ) supplier);
         throw new IllegalArgumentException("Unknown Supplier subtype: " + supplier.getClass());
     }
 
