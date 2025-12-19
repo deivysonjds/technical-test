0d30f92da12507ddb032757dd1a8c34cefb6e10a
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-19 02:32:47 -0300

    feat: update service

--- frontend/services/enterpriseService.ts
+++ frontend/services/enterpriseService.ts
@@ -1,14 +1,14 @@
-import { EnterpriseFormData, enterpriseResponse } from "@/types/enterprise";
+import { enterprise, EnterpriseFormData, enterpriseSummary } from "@/types/enterprise";
 import api from "./api";
 import { Page } from "@/types/page";
 
 const enterpriseService = {
-    getAll: async (): Promise<Page<enterpriseResponse>>=>{
-        let response = await api.get<Page<enterpriseResponse>>('/enterprises/')
+    getAll: async (): Promise<Page<enterprise>>=>{
+        let response = await api.get<Page<enterprise>>('/enterprises/')
         return response.data
     },
 
-    insert: async(enterprise: EnterpriseFormData): Promise<enterpriseResponse>=>{
+    insert: async(enterprise: EnterpriseFormData): Promise<enterpriseSummary>=>{
         let response = await api.post('/enterprises/',enterprise)
         
         return response.data
@@ -17,6 +17,11 @@ const enterpriseService = {
     delete: async(id: number): Promise<void> =>{
         await api.delete(`/enterprises/${id}`)
         return
+    },
+    update: async(id: number, enterprise: EnterpriseFormData): Promise<enterpriseSummary> =>{
+
+        let response = await api.put<enterpriseSummary>(`/enterprises/${id}`, enterprise)
+        return response.data
     }
 }
 
