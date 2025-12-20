4a4cb97846031b2c6b90e8021c3bf115686bed7e
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-20 00:40:17 -0300

    feat: added selectedEnterprise in zustand

--- frontend/store/enterpriseStore.ts
+++ frontend/store/enterpriseStore.ts
@@ -3,14 +3,20 @@ import { create } from "zustand"
 
 interface EnterpriseStore {
   enterprises: enterprise[]
+  selectedEnterprise: enterprise | null
   setAll: (enterprises: enterprise[]) => void
+  setSelectedEnterprise: (enterprise: enterprise) => void
 }
 
 export const useEnterprisesStore = create<EnterpriseStore>((set) => ({
   enterprises: [],
-
+  selectedEnterprise: null,
   setAll: (enterprises) =>
     set({
       enterprises:enterprises
     }),
+  setSelectedEnterprise: (enterprise) => 
+    set({
+      selectedEnterprise: enterprise
+    })
 }))
