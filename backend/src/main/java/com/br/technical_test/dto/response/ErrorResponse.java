e86c33db61a61d5fbde8c7fdaa7423f4f7e7033e
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-23 00:49:20 -0300

    feat: error response controller

--- backend/src/main/java/com/br/technical_test/dto/response/ErrorResponse.java
+++ backend/src/main/java/com/br/technical_test/dto/response/ErrorResponse.java
@@ -1,4 +1,14 @@
 package com.br.technical_test.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
+    private String message;
 }
