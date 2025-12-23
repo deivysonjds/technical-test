2b8e0f287c3da77e9bf7324f79c780321f9f6202
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-23 00:59:22 -0300

    feat: services to suppliers

---
+++ frontend/services/supplierService.ts
@@ -0,0 +1,50 @@
+import api from "./api";
+import { Page } from "@/types/page";
+import { 
+    supplierPf, 
+    supplierPfFormData, 
+    supplierPfSummary, 
+    supplierPj, 
+    supplierPjFormData, 
+    supplierPjSummary 
+} from "@/types/supplier";
+
+const supplierService = {
+    getAll: async (): Promise<Page<supplierPf | supplierPj>>=>{
+        let response = await api.get<Page<supplierPf | supplierPj>>('/suppliers/')
+        return response.data
+    },
+    findById: async(id: number): Promise<supplierPf | supplierPj> => {
+        let response = await api.get<supplierPf | supplierPj>(`/suppliers/${id}`)
+
+        return response.data
+    },
+
+    insertPf: async(supplierPf: supplierPfFormData): Promise<supplierPfSummary>=>{
+        let response = await api.post<supplierPfSummary>('/suppliers/pf',supplierPf)
+        
+        return response.data
+    },
+    insertPj: async(supplierPj: supplierPjFormData): Promise<supplierPjSummary>=>{
+        let response = await api.post<supplierPjSummary>('/suppliers/pj',supplierPj)
+        
+        return response.data
+    },
+
+    delete: async(id: number): Promise<void> =>{
+        await api.delete(`/suppliers/${id}`)
+        return
+    },
+    updatePf: async(id: number, supplierPf: supplierPfFormData): Promise<supplierPf> =>{
+
+        let response = await api.put<supplierPf>(`/suppliers/pf/${id}`, supplierPf)
+        return response.data
+    },
+    updatePj: async(id: number, supplierPj: supplierPjFormData): Promise<supplierPj> =>{
+
+        let response = await api.put<supplierPj>(`/suppliers/pj/${id}`, supplierPj)
+        return response.data
+    }
+}
+
+export default supplierService;
