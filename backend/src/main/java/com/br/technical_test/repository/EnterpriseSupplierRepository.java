1b377e12478351d2fc480c5bb82097e36b2b01ee
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-23 00:55:19 -0300

    feat: find by supplierId and enterpriseId

--- backend/src/main/java/com/br/technical_test/repository/EnterpriseSupplierRepository.java
+++ backend/src/main/java/com/br/technical_test/repository/EnterpriseSupplierRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EnterpriseSupplierRepository extends JpaRepository<EnterpriseSupplier, Long> {
     List<EnterpriseSupplier> findByEnterpriseId(Long enterpriseId);
     List<EnterpriseSupplier> findBySupplierId(Long supplierId);
+    Optional<EnterpriseSupplier> findBySupplierIdAndEnterpriseId(Long supplierId, Long enterpriseId);
     boolean existsByEnterpriseIdAndSupplierId(Long enterpriseId, Long supplierId);
 }
