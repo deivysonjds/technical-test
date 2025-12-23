ceab23c344cd2870d75967471175971affe7d790
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-12-23 00:58:33 -0300

    feat: added forms schemas with zod to supplier PJ and PF

--- frontend/types/supplier.ts
+++ frontend/types/supplier.ts
@@ -1,3 +1,4 @@
+import z from "zod"
 import { enterpriseSummary } from "./enterprise"
 
 
@@ -38,3 +39,52 @@ export interface supplierPjSummary {
     cep: string,
     cnpj: string,
 }
+
+export const supplierPfSchema = z.object({
+    name: z
+        .string()
+        .min(1, "Nome é obrigatório"),
+
+    cep: z
+        .string()
+        .regex(/^\d{8}$/, "CEP inválido"),
+
+    cpf: z
+        .string()
+        .regex(
+            /^\d{11}$/,
+            "CPF inválido"
+        ),
+    email: z
+        .email({ error: "E-mail inválido" }),
+    rg: z
+        .string()
+        .regex(
+            /^\d{7,9}$/,
+            "RG inválido"
+        ),
+    birthDate: z
+        .date({ error: "Data inválida" })
+})
+
+export const supplierPjSchema = z.object({
+    name: z
+        .string()
+        .min(1, "Nome é obrigatório"),
+
+    cep: z
+        .string()
+        .regex(/^\d{8}$/, "CEP inválido"),
+
+    cnpj: z
+        .string()
+        .regex(
+            /^\d{14}$/,
+            "CNPJ inválido"
+        ),
+    email: z
+        .email({ error: "E-mail inválido" }),
+})
+
+export type supplierPfFormData = z.infer<typeof supplierPfSchema>
+export type supplierPjFormData = z.infer<typeof supplierPjSchema>
